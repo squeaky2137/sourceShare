8224fe8a55db4f0fff11ec166c77d4a7062a9a96
Squeaky2137 <b.bronaugh@yahoo.com>
2023-03-17 18:25:12 -0500

    fix: eslint and prettier formatting

--- src/__tests__/sourcebin.test.ts
+++ src/__tests__/sourcebin.test.ts
@@ -1,40 +1,40 @@
-import { SRCreate, SRGet } from '../index';
-test('creator1', async () => {
-  const resp = await SRCreate('sourceBin', {
-    title: 'Test',
-    description: 'JEST TEST ONE',
-    language: 'javascript',
-    files: [{ name: 'code', content: 'test' }],
+import { SRCreate, SRGet } from "../index";
+test("creator1", async () => {
+  const resp = await SRCreate("sourceBin", {
+    title: "Test",
+    description: "JEST TEST ONE",
+    language: "javascript",
+    files: [{ name: "code", content: "test" }],
   });
   expect(resp).toBeDefined();
 });
 
-test('creator2', async () => {
-  const resp = <string>await SRCreate('sourceBin', {
-    title: 'Test2',
-    description: 'JEST TEST TWO',
-    files: [{ name: 'code', content: 'trolololololol', languageId: 'javascript' }],
+test("creator2", async () => {
+  const resp = <string> await SRCreate("sourceBin", {
+    title: "Test2",
+    description: "JEST TEST TWO",
+    files: [{ name: "code", content: "trolololololol", languageId: "javascript" }],
   });
-  const getter = await SRGet('sourceBin', resp);
+  const getter = await SRGet("sourceBin", resp);
   console.log(getter);
-  expect(getter.title).toBe('Test2');
+  expect(getter.title).toBe("Test2");
   expect(resp).toBeDefined();
 });
 
-test('creator3', async () => {
-  const resp = await SRCreate('sourceBin', {
-    files: [{ name: 'code', content: 'test' }],
+test("creator3", async () => {
+  const resp = await SRCreate("sourceBin", {
+    files: [{ name: "code", content: "test" }],
   });
   expect(resp).toBeDefined();
 });
 
-test('creator4ERROR', async () => {
+test("creator4ERROR", async () => {
   try {
-    await SRCreate('sourceBin', {
-      language: 'rust',
+    await SRCreate("sourceBin", {
+      language: "rust",
       files: [
-        { name: 'code', content: 'test' },
-        { name: 'code2', content: 'test2' },
+        { name: "code", content: "test" },
+        { name: "code2", content: "test2" },
       ],
     });
     expect(true).toBe(false);
--- src/__tests__/srcshare.test.ts
+++ src/__tests__/srcshare.test.ts
@@ -1,47 +1,47 @@
-import { SRCreate, SRGet } from '../index';
-test('creator1', async () => {
-  const resp = await SRCreate('srcShare', {
-    title: 'Test',
-    description: 'JEST TEST ONE',
-    language: 'javascript',
-    files: [{ name: 'code', content: 'test' }],
+import { SRCreate, SRGet } from "../index";
+test("creator1", async () => {
+  const resp = await SRCreate("srcShare", {
+    title: "Test",
+    description: "JEST TEST ONE",
+    language: "javascript",
+    files: [{ name: "code", content: "test" }],
   });
   expect(resp).toBeDefined();
 });
 
-test('creator2', async () => {
-  const resp = <string>await SRCreate('srcShare', {
-    title: 'Test2',
-    description: 'JEST TEST TWO',
-    language: 'javascript',
+test("creator2", async () => {
+  const resp = <string> await SRCreate("srcShare", {
+    title: "Test2",
+    description: "JEST TEST TWO",
+    language: "javascript",
     files: [
-      { name: 'code', content: 'test' },
-      { name: 'code2', content: 'test2' },
+      { name: "code", content: "test" },
+      { name: "code2", content: "test2" },
     ],
   });
-  const getter = await SRGet('srcShare', resp);
+  const getter = await SRGet("srcShare", resp);
   console.log(getter);
-  expect(getter.title).toBe('Test2');
+  expect(getter.title).toBe("Test2");
   expect(resp).toBeDefined();
 });
 
-test('creator3', async () => {
-  const resp = await SRCreate('srcShare', {
+test("creator3", async () => {
+  const resp = await SRCreate("srcShare", {
     files: [
-      { name: 'code', content: 'test' },
-      { name: 'code2', content: 'test2' },
+      { name: "code", content: "test" },
+      { name: "code2", content: "test2" },
     ],
   });
   expect(resp).toBeDefined();
 });
 
-test('creator4ERROR', async () => {
+test("creator4ERROR", async () => {
   try {
-    await SRCreate('srcShare', {
-      language: 'notalanguage',
+    await SRCreate("srcShare", {
+      language: "notalanguage",
       files: [
-        { name: 'code', content: 'test' },
-        { name: 'code2', content: 'test2' },
+        { name: "code", content: "test" },
+        { name: "code2", content: "test2" },
       ],
     });
     expect(true).toBe(false);
